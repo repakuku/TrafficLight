a2a35344998af59849f87a4ed2d0ff2161709e20
Alexey Turulin <repakuku@icloud.com>
2023-04-22 17:02:48 -0400

    Completed the task "Traffic Light"

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -1,19 +1,45 @@
-//
-//  ViewController.swift
-//  TrafficLight
-//
-//  Created by Алексей Турулин on 4/22/23.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private var counter = 0
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        
+        button.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func buttonPressed() {
+        button.setTitle("NEXT", for: .normal)
+        
+        switch counter % 3 {
+        case 0:
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+        case 1:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        case 2:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        default:
+            break
+        }
+        
+        counter += 1
+    }
 }
 
